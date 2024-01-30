7a4a023458fa6b137f75a28d3b04a389790775eb
jtech mind <jtechmind@gmail.com>
2024-01-30 13:41:23 +0530

    fetch product categories in header withoud css

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Header() {
+  const [headerOptions, setHeaderOptions] = useState([]);
+
+  useEffect(() => {
+    const getOptions = async () => {
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products/categories"
+        );
+
+        if (!response.ok) {
+          throw new Error("");
+        }
+        const data = await response.json();
+        setHeaderOptions(data);
+      } catch (error) {
+        console.log("Error in fatching data: ", error);
+      }
+    };
+    getOptions();
+  }, []);
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div>
+        <nav>
+          <ul>
+            {headerOptions.map((option) => (
+              <li key={option.id}>
+                <a href={option}> {option}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
